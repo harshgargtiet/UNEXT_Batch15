c3d27a2176d5a6a32df78b22c67c7e575e5f7e15
K-Rage <sbhadauria_be19@thapar.edu>
2023-09-06 08:44:55 +0000

    Added Js files and assignment

---
+++ Shaurya_Pratap_Singh/HTML_CSS_Bootstrap/JS/objects.js
@@ -0,0 +1,22 @@
+
+    // Creating an object
+const person = {
+  pname: "John",
+  age: 30,
+  city: "New York"
+};
+
+// Accessing object properties
+const pname = person.pname;
+const age = person["age"];
+
+// Modifying object properties
+person.age = 31;
+person["city"] = "Los Angeles";
+
+// Displaying object data
+console.log("Person:", person);
+console.log("Name:", pname);
+console.log("Age:", age);
+
+
---
+++ Shaurya_Pratap_Singh/HTML_CSS_Bootstrap/JS/operators.js
@@ -0,0 +1,11 @@
+// Operators and Expressions
+const a = 5;
+const b = 3;
+
+const sum = a + b;
+const product = a / b;
+
+console.log("Sum:", sum);
+console.log("Product:", product);
+
+alert("Sum"+' '+sum);
