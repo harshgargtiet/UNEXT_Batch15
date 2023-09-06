324a8d1ccab92f3c372ec01a61fb4957aa7c1802
NamanJain1902 <1902ben10@gmail.com>
2023-09-06 06:54:30 +0000

    w2 d3

---
+++ Naman_Jain/Frontend/JS/objects.js
@@ -0,0 +1,24 @@
+
+    // Creating an object
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+// Accessing object properties
+const name = person.name;
+const age = person["age"];
+
+console.log("Person:", person);
+
+// Modifying object properties
+person.age = 31;
+person["city"] = "Los Angeles";
+
+// Displaying object data
+console.log("Person:", person);
+console.log("Name:", name);
+console.log("Age:", age);
+
+
---
+++ Naman_Jain/Frontend/JS/operators.js
@@ -0,0 +1,12 @@
+// Operators and Expressions
+const a = 5;
+const b = 3;
+
+var sum = a + b;
+const product = a * b;
+const div = Math.floor(a / b);
+// sum = '0' + (product + a) + '32' + 0;
+
+console.log("Sum:", sum);
+console.log("Product:", product);
+console.log("div:", div);
