1421ba3a22555068e45d8eb1ed69ebabfb4021ab
harshgargtiet <harshgargthapar@gmail.com>
2023-09-06 06:05:31 +0000

    added ui code

---
+++ HarshGargUI/JS/objects.js
@@ -0,0 +1,22 @@
+
+    // Creating an object
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+// Accessing object properties
+const name = person.name;
+const age = person["age"];
+
+// Modifying object properties
+person.age = 31;
+person["city"] = "Los Angeles";
+
+// Displaying object data
+console.log("Person:", person);
+console.log("Name:", name);
+console.log("Age:", age);
+
+
