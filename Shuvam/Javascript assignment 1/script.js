37dc05098bef13388a3aa804b88878e76f9cef32
coffeeCoder69 <mandalshuvam71@gmail.com>
2023-09-06 12:31:10 +0530

    hi

--- Shuvam/Javascript assignment 1/script.js
+++ Shuvam/Javascript assignment 1/script.js
@@ -1,16 +1,42 @@
-// Function to change the background color of a specified element
-function changeColor(elementId, color) {
+// Function to generate a random color in hexadecimal format
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to change the background color of a specified element to a random color
+function changeColor(elementId) {
     const element = document.getElementById(elementId);
     if (element) {
-        element.style.backgroundColor = color;
+        const randomColor = getRandomColor();
+        element.style.backgroundColor = randomColor;
     }
 }
 
-// Function to add text content to a specified element
-function addText(elementId, text) {
+// Function to generate random text
+function getRandomText() {
+    const texts = [
+        "Random Text 1",
+        "Random Text 2",
+        "Random Text 3",
+        "Lorem Ipsum",
+        "JavaScript is fun",
+        "Hello, World!",
+    ];
+    const randomIndex = Math.floor(Math.random() * texts.length);
+    return texts[randomIndex];
+}
+
+// Function to change the text content of a specified element to random text
+function changeText(elementId) {
     const element = document.getElementById(elementId);
     if (element) {
-        element.textContent = text;
+        const randomText = getRandomText();
+        element.textContent = randomText;
     }
 }
 
