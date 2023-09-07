ed16e0ddc8a6dc475da4f36ade54a20ebe6bc593
Vridhi Kamath <vridhi.vk@gmail.com>
2023-09-07 06:03:17 +0000

    Added files

---
+++ vridhi/DBF/DBF/Models/CRUDOperations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBF.Models
+{
+    internal class CRUDOperations
+    {
+
+        public void InsertRecords()
+        {
+            using(var context = new StudentMgmtContext())
+            {
+                var stud1 = new Student()
+                {
+                    Rollno = 10001,
+                    Name = "Rahul",
+                    City ="BLR"
+                };
+
+                var stud2 = new Student()
+                {
+                    Rollno = 10002,
+                    Name = "Vivek",
+                    City = "BLR"
+                };
+                context.Students.Add(stud1);
+                context.Students.Add(stud2);
+                context.SaveChanges();
+            }
+        }
+        public void ReadDataFromStudentEntity(string stname)
+        {
+            var context = new StudentMgmtContext();
+            var st = context.Students.Where
+                (s => s.Name == stname);
+            foreach (var s in st)
+            {
+                Console.WriteLine(s.Name);
+            }
+        }
+    }
+}
---
+++ vridhi/DBF/DBF/Models/Dummy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBF.Models;
+
+public partial class Dummy
+{
+    public int? Id { get; set; }
+
+    public string? Name { get; set; }
+}
